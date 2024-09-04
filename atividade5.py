ea6e5bac9b6ba24a574e4e1eb146b4456b09e84b
Henry2678 <soldadohuefl@gmail.com>
2024-09-04 16:08:58 -0300

    atividade5.py

--- atividade5.py
+++ atividade5.py
@@ -1,4 +1,4 @@
 # Escreva um programa que receba um valor em minutos e converta para segundos.
-valor = float(input("digite o valor em horas "))
-conversao = valor*3600
+valor = float(input("digite o valor em minutos "))
+conversao = valor*60
 print(f"o valor em segundos é {conversao}")
